ca3732f6f127d23ee7ceecc6fbba67989a261424
Gabriel Soares <gabrielrs.job@gmail.com>
2023-11-15 20:49:04 -0300

    feat: inputs e titulo implementados além de evento base para fechar o modal ao 'cadastrar'

--- src/components/NewNoteModal/index.js
+++ src/components/NewNoteModal/index.js
@@ -1,9 +1,16 @@
 import CustomModal from "../CustomModal";
+import NoteForm from "../NoteForm";
 
-export default function NewNoteModal({visible}) {
+import { Title } from "./style";
+
+export default function NewNoteModal({ visible, onSave, onClose }) {
     return (
-        <CustomModal visible={visible}>
-
+        <CustomModal 
+        visible={visible}
+        onClose={onClose}
+        >
+            <Title>Cadastrar nova nota</Title>
+            <NoteForm onClose={onClose}/>
         </CustomModal>
     );
 }
