03f0f6538d134e66427383293618091959a54499
Douglas Andrade <suportdoug@gmai.com>
2023-11-15 21:45:37 -0300

    features/card

--- src/main/index.js
+++ src/main/index.js
@@ -1,10 +1,12 @@
 import CustomHeader from '../components/Header';
+import CardContent from '../components/Card';
 import { Container } from './styles';
 
 export default function Main() {
   return (
     <Container>
         <CustomHeader />
+        <CardContent />
     </Container>
   );
 }
