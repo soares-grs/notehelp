df71e05a8f8648371f23688a6fabb79f46dbb287
Douglas Andrade <suportdoug@gmai.com>
2023-11-17 19:48:28 -0300

    button/att

--- src/components/AddNoteButton/styles.js
+++ src/components/AddNoteButton/styles.js
@@ -1,24 +1,24 @@
 import styled from "styled-components";
 
 export const Container = styled.TouchableOpacity`
-    border-radius: 100px;
-    width: 62px;
-    height: 62px;
-    background-color: #252525;
     position: absolute;
     bottom: 0;
     right: 0;
-    margin: 12px;
-    align-items: center;
-    display: flex;
-    justify-content: center;
+    margin: 0 10px 30px 0;
 `;
 
 export const PlusIcon = styled.Text`
     font-size: 38px;
     color: #d9d9d9;
     font-family: 'Montserrat-400';
-    align-items: center;
-    display: flex;
-    justify-content: center;
+`;
+
+export const Circle = styled.View`
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    background-color: #252525;
+    border-radius: 100px;
+    width: 52px;
+    height:52px;
 `;
