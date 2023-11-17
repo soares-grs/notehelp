ce8fc1762b17262ba84532f2803bf3cec927833c
Douglas Andrade <suportdoug@gmai.com>
2023-11-17 12:45:16 -0300

    Feature/Sections + Alteração posição de bloco(Dinamicamente)

--- src/main/index.js
+++ src/main/index.js
@@ -1,9 +1,12 @@
+import { Container } from './styles';
+import { useState } from 'react';
 import CustomHeader from '../components/Header';
 import CardContent from '../components/Card';
-import { Container } from './styles';
 import AddNoteButton from '../components/AddNoteButton';
-import { useState } from 'react';
 import NewNoteModal from '../components/NewNoteModal';
+import SectionNotas from '../components/SectionNotas';
+import SectionFixed from '../components/SectionFixed';
+
 
 export default function Main() {
 
@@ -12,13 +15,15 @@ export default function Main() {
 
   return (
     <Container>
-        <CustomHeader />
-        <CardContent />
-        <AddNoteButton onPress={() => setIsNewNoteModalVisible(true)} />
-        <NewNoteModal
-          visible={isNewNoteModalVisible}
-          onClose={() => setIsNewNoteModalVisible(false)}
-          // onSave={handleCreateNote}
+      <CustomHeader />
+      <SectionFixed />
+      <CardContent />
+      <SectionNotas />
+      <AddNoteButton onPress={() => setIsNewNoteModalVisible(true)} />
+      <NewNoteModal
+        visible={isNewNoteModalVisible}
+        onClose={() => setIsNewNoteModalVisible(false)}
+      // onSave={handleCreateNote}
       />
     </Container>
   );
